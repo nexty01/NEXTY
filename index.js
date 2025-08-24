e8f873d1967186050d6c20d374df2481284bbf10
nexty01 <fakejeson@gmail.com>
2025-08-24 21:29:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -10,6 +8,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "friendship.html"));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
