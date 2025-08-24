d39c2d2055e726538a2b4cbc34af6c4f6250a50e
nexty01 <fakejeson@gmail.com>
2025-08-24 21:31:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// static files serve karne ke liye
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "friendship.html"));
+  res.sendFile(path.join(__dirname, "friendship.html"));
 });
 
 const PORT = process.env.PORT || 3000;
